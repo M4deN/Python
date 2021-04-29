c0c4e6f4959937cc9c3b8fcfeb09345b73d8ce9e
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-04-29 11:31:50 -0300

    Update Multiplos.py

--- Simples/Multiplos.py
+++ Simples/Multiplos.py
@@ -1,8 +1,5 @@
-def main():
+a = int(input())
 
-    a = int(input())
-
-    for i in range(1,11):
-        total = i*a
-        print('{} x {} = {}'.format(i, a,total))
-main()
+for i in range(1,11):
+    total = i*a
+    print('{} x {} = {}'.format(i, a,total))
