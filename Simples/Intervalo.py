a0123f5906a444721d9c54c42b756b41ff7c37e6
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-04-29 11:31:18 -0300

    Update Intervalo.py

--- Simples/Intervalo.py
+++ Simples/Intervalo.py
@@ -1,19 +1,17 @@
-def main():
-    a = float(input())
+a = float(input())
 
-    if 0 <= a <= 25.00:
-        print("Intervalo [0,25]")
+if 0 <= a <= 25.00:
+     print("Intervalo [0,25]")
 
-    elif 25.00 < a <= 50.00:
-        print("Intervalo (25,50]")
+elif 25.00 < a <= 50.00:
+     print("Intervalo (25,50]")
 
-    elif 50.00 < a <= 75.00:
-        print("Intervalo (50,75]")
+elif 50.00 < a <= 75.00:
+     print("Intervalo (50,75]")
 
-    elif 75.00 < a <= 100.00:
-        print("Intervalo (75,100]")
+elif 75.00 < a <= 100.00:
+     print("Intervalo (75,100]")
 
-    elif a > 100.00 or a < 0:
+elif a > 100.00 or a < 0:
         print("Fora de intervalo")
 
-main()
