28adfffed1982c6ce4defc4f0825b16e5001519d
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-05-01 08:27:26 -0300

    Create PDF_Com_Senhas.py

---
+++ Simples/PDF_Com_Senhas.py
@@ -0,0 +1,19 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import sys
+
+def secure_pdf(file, password):
+    parser = PdfFileWriter()
+    pdf = PdfFileReader(file)
+    for page in range(pdf.numPages):
+         parser.addPage(pdf.getPage(page))
+    parser.encrypt(password)
+    with open(f"secure_{file}", "wb") as f:
+        parser.write(f)
+        f.close()
+    print(f"secure_{file} Created...")
+
+
+if __name__ == "__main__":
+     file = sys.argv[certificado2.pdf]
+     password = sys.argv[2]
+     secure_pdf(file, password)
